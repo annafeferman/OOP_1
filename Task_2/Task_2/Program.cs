ae18f3eac244f630e6ebe6a0778f9d54f9e0b7f8
Анна Феферман <feferman.a@donnu.edu.ua>
2020-11-01 13:02:20 +0200

    first commit

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter c: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("a = {0}; b = {1}; c = {2}", a, b, c);
+
+
+        }
+    }
+}
